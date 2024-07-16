873ffaf0ab3827625fb8193798fd9e502fff2f88
akashkumar9508 <akashbca9508@gmail.com>
2024-07-16 22:25:09 +0530

    loader added

--- script.js
+++ script.js
@@ -1,31 +1,27 @@
+// script.js
+window.addEventListener('load', function() {
+    var loader = document.getElementById('loader');
+    var content = document.getElementById('content');
+    setTimeout(function() {
+        loader.style.display = 'none';
+        content.style.display = 'block';
+        initializeScrollTrigger();
+    }, 2000); 
+});
+function initializeScrollTrigger() {
+    const slideCont = document.querySelector(".slideCont");
+    const section2 = document.querySelector(".section2");
 
-
-// horizontal scroll
-// gsap.to(".slide",{
-//     scrollTrigger : {
-//         trigger : ".real",
-//         start : "top top",
-//         end : "bottom bottom", 
-//         scrub : 1
-//     },
-//     xPercent : -200,
-//     ease :Power4
-// });
-
-
-const slideCont = document.querySelector(".slideCont");
-const section2 = document.querySelector(".section2");
-
-gsap.to(slideCont,{
-    transform:"translateX(-201.5%)",
-    scrollTrigger:{
-        trigger: section2,
-        scroller:"body",
-        // markers:true,
-        start:"top 0%",
-        end:"top -200%",
-        scrub:2,
-        pin:true
-
-    }
-})
+    gsap.to(slideCont, {
+        transform: "translateX(-200%)",
+        scrollTrigger: {
+            trigger: section2,
+            scroller: "body",
+            // markers: true,
+            start: "top 0%",
+            end: "top -200%",
+            scrub: 2,
+            pin: true
+        }
+    });
+}
