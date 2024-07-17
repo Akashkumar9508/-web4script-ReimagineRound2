d198b071e193b7e3137df7f6e44c554b8810a11d
akashkumar9508 <akashbca9508@gmail.com>
2024-07-17 13:12:06 +0530

    animation on first section

--- script.js
+++ script.js
@@ -8,6 +8,22 @@ window.addEventListener('load', function() {
         initializeScrollTrigger();
     }, 200); 
 });
+
+// for responsive nav bar 
+let lastScrollTop =0;
+navbar = document.querySelector(".nav");
+window.addEventListener("scroll",function(){
+  var scrollTop = window.pageYOffset || this.document.elementFromPoint.scrollTop;
+  if(scrollTop > lastScrollTop){
+    navbar.style.top="-10%";
+  }
+  else{
+    navbar.style.top="0%";
+  }
+  lastScrollTop = scrollTop;
+})
+
+// gsap 
 function initializeScrollTrigger() {
     const slideCont = document.querySelector(".slideCont");
     const section2 = document.querySelector(".section2");
@@ -24,14 +40,56 @@ function initializeScrollTrigger() {
             pin: true
         }
     });
-}
+    const threeDModle = document.querySelector(".threeDModle");
+    const section1= document.querySelector(".section1");
+    const section3= document.querySelector(".section3");
+    const nav= document.querySelector(".nav");
+
+    const tl1 = gsap.timeline();
+    tl1.from(".threeDModle",{
+      y:600,
+      // rotate:"360deg",
+      duration:2,
+      ease: "elastic.out(1,0.3)",
+    });
+    tl1.from(".circle",{
+      opacity:0,
+      duration:2,
+      ease:"power1.inout",
+    });
+    tl1.from(".mainText1",{
+      x:-500,
+      opacity:0,
+      duration:1,
+      ease:"power1.inout",
+    },'mainText');
+    tl1.from(".mainText2",{
+      x:500,
+      opacity:0,
+      duration:1,
+      ease:"power1.inout",
+    },'mainText');
+    tl1.from(nav,{
+        y: -100,
+        opacity:0,
+        duration: 1, // duration of 1 second
+        ease: "power1.inOut" // easing function for smooth animation
+      });
+
+      tl1.from(".iconSection1",{
+        x:-100,
+        opacity:0,
+        duration:1,
+        ease:"power1.inout",
+      },'icon');
+      tl1.from(".iconSection2",{
+        right:-100,
+        opacity:0,
+        duration:1,
+        ease:"power1.inout",
+      },'icon');
+    };
 
 
-// 3d model animation 
-
-const threeDModle = document.querySelector(".3dModle");
-const section1= document.querySelector(".section1");
-const section2= document.querySelector(".section2");
-const section3= document.querySelector(".section3");
 
 
