c72cd274bf2db6de639acf230b5abffe98b93825
Anant kumar <anant1042005@gmail.com>
2024-07-19 00:56:06 +0530

    added some responsiveness

--- script.js
+++ script.js
@@ -152,40 +152,50 @@ function initializeScrollTrigger(){
 
 
 //cursor animation
-var cursor =document.querySelector('.cursor'),
-        cursorScale = document.querySelectorAll('.cursor-scale'),
-         mouseX = 0;
-         mouseY = 0;
-         gsap.to({}, 0.016,{
-            repeat:-1,
-            onRepeat:function(){
-                gsap.set(cursor, {
-                    css:{
-                        left:mouseX,
-                        top:mouseY
-                    }
-                })
+// Function to initialize cursor effects
+function initializeCursorEffects() {
+  var cursor = document.querySelector('.cursor'),
+      cursorScale = document.querySelectorAll('.cursor-scale'),
+      mouseX = 0,
+      mouseY = 0;
 
-            }
-         });
-         window.addEventListener("mousemove" ,function (e){
-            mouseX = e.clientX;
-            mouseY = e.clientY;
-         })
-         cursorScale.forEach(link => {
-            link.addEventListener('mouseleave', ()=> {
-                cursor.classList.remove('grow');
-                cursor.classList.remove('grow-small');
+  gsap.to({}, 0.016, {
+      repeat: -1,
+      onRepeat: function() {
+          gsap.set(cursor, {
+              css: {
+                  left: mouseX,
+                  top: mouseY
+              }
+          });
+      }
+  });
 
-            });
-            link.addEventListener('mousemove', ()=> {
-                cursor.classList.add('grow');
-                if(link.classList.contains('small')){
-                    cursor.classList.remove('grow');
-                    cursor.classList.add('grow-small');
-                }
-            });
-        })
+  window.addEventListener("mousemove", function(e) {
+      mouseX = e.clientX;
+      mouseY = e.clientY;
+  });
+
+  cursorScale.forEach(link => {
+      link.addEventListener('mouseleave', () => {
+          cursor.classList.remove('grow');
+          cursor.classList.remove('grow-small');
+      });
+      link.addEventListener('mousemove', () => {
+          cursor.classList.add('grow');
+          if (link.classList.contains('small')) {
+              cursor.classList.remove('grow');
+              cursor.classList.add('grow-small');
+          }
+      });
+  });
+}
+
+// Check if the screen width is greater than 768px (desktop)
+if (window.matchMedia("(min-width: 768px)").matches) {
+  initializeCursorEffects();
+}
+
 
 
 
