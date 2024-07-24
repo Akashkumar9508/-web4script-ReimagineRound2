38b8695df87fa886570cd0c0e9ff4e169f0bb0b4
Anant kumar <anant1042005@gmail.com>
2024-07-24 22:59:28 +0530

    added some redefined changes

--- script.js
+++ script.js
@@ -23,7 +23,7 @@ window.addEventListener('load', function () {
     loader.style.display = 'none';
     content.style.display = 'block';
     initializeScrollTrigger();
-  },100);
+  },10000);
 });
 
 // for responsive nav bar 
